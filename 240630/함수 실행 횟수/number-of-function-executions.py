b18089d26bc0d0cda2ac71ef74dc30e2891e03a6
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-06-30 23:41:36 +0900

    Title: 함수 실행 횟수, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240630/함수 실행 횟수/number-of-function-executions.py
+++ 240630/함수 실행 횟수/number-of-function-executions.py
@@ -9,10 +9,10 @@ if __name__=="__main__":
     else:
         memo = [0 for _ in range(n+1)]
         memo[1] = 1
-        memo[2] = 2
+        memo[2] = 3
         
         for i in range(3, n+1):
-            memo[i] = memo[i-1] + memo[i-2]
+            memo[i] = memo[i-1] + memo[i-2] + 1
             memo[i] %= 1000000007
 
-        print((memo[n] + memo[n-1]) % 1000000007)
+        print((memo[n]) % 1000000007)
