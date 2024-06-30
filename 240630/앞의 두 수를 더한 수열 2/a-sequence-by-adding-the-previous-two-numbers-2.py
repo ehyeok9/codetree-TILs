a1df3012f69a36fb2a04c89d736b32e5f6f0e1f2
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-06-30 23:07:00 +0900

    Title: 앞의 두 수를 더한 수열 2, Time: 98ms, Memory: 24MB, Status: Passed - CodeTree

--- 240630/앞의 두 수를 더한 수열 2/a-sequence-by-adding-the-previous-two-numbers-2.py
+++ 240630/앞의 두 수를 더한 수열 2/a-sequence-by-adding-the-previous-two-numbers-2.py
@@ -4,12 +4,14 @@ input = sys.stdin.readline
 
 if __name__=="__main__":
     n = int(input())
-    
-    memo = [0 for _ in range(n+1)]
-    memo[1] = 1
-    memo[2] = 1
+    if n <= 2:
+        print(1)
+    else:
+        memo = [0 for _ in range(n+1)]
+        memo[1] = 1
+        memo[2] = 1
 
-    for i in range(3, n+1):
-        memo[i] = memo[i-1] + memo[i-2]
-    
-    print(memo[n])
+        for i in range(3, n+1):
+            memo[i] = memo[i-1] + memo[i-2]
+        
+        print(memo[n])
