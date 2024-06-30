307df085f02fe6bd7c63e5c34e07916f09f67694
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-06-30 23:05:25 +0900

    Title: 앞의 두 수를 더한 수열 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240630/앞의 두 수를 더한 수열 2/a-sequence-by-adding-the-previous-two-numbers-2.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline 
+
+if __name__=="__main__":
+    n = int(input())
+    
+    memo = [0 for _ in range(n+1)]
+    memo[1] = 1
+    memo[2] = 1
+
+    for i in range(3, n+1):
+        memo[i] = memo[i-1] + memo[i-2]
+    
+    print(memo[n])
