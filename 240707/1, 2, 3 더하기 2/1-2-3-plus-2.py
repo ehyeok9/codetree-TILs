a92a665cb60b4ff2aa9112d08ea315ba46504e90
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-07-07 19:10:54 +0900

    Title: 1, 2, 3 더하기 2, Time: 169ms, Memory: 28MB, Status: Passed - CodeTree

--- 240707/1, 2, 3 더하기 2/1-2-3-plus-2.py
+++ 240707/1, 2, 3 더하기 2/1-2-3-plus-2.py
@@ -7,11 +7,11 @@ def dp(n):
     memo[1][1] = memo[2][2] = memo[3][1] = memo[3][2] = memo[3][3] = 1
     
     for i in range(4, n+1):
-        memo[i][1] = memo[i-1][2] + memo[i-1][3]
-        memo[i][2] = memo[i-2][1] + memo[i-2][3]
-        memo[i][3] = memo[i-3][1] + memo[i-3][2]
+        memo[i][1] = (memo[i-1][2] + memo[i-1][3]) % 1000000007
+        memo[i][2] = (memo[i-2][1] + memo[i-2][3]) % 1000000007
+        memo[i][3] = (memo[i-3][1] + memo[i-3][2]) % 1000000007
 
-    return sum(memo[n])
+    return sum(memo[n]) % 1000000007
     
 
 if __name__=="__main__":
