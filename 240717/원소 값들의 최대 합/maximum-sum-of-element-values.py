8011b53bed003ec7f89d195e479850d118d4aed8
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-07-17 17:39:25 +0900

    Title: 원소 값들의 최대 합, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240717/원소 값들의 최대 합/maximum-sum-of-element-values.py
+++ 240717/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     sequence.insert(0, 0)
 
     answer = 0
-    for i in range(1,n):
+    for i in range(1,n+1):
         temp = 0
         for j in range(m):
             temp += moving(sequence, i)
