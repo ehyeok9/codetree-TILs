7a6f0ed38345fc00cee0f8318692cffc1e50cf20
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-01 20:18:36 +0900

    Title: 수 채우기, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240801/수 채우기/fill-in-number.py
+++ 240801/수 채우기/fill-in-number.py
@@ -4,22 +4,14 @@ input = sys.stdin.readline
 
 if __name__=="__main__":
     n = int(input())
-    memo = [0 for _ in range(n+1)]
-    memo[1] = -1
+    
+    answer = float("inf")
+    for i in range(n//5 + 1):
+        remainder = (n - 5 * i)
+        if remainder % 2 == 0:
+            answer = min(answer, i + remainder//2)
+    
+    if answer == float("inf"):
+        answer = -1
 
-    for i in range(2, n+1):
-        if i == 2 or i == 5: memo[i] = 1
-        if i == 4: memo[i] = 2
-        if i == 3: memo[i] = -1
-        if i >= 6: 
-            if memo[i-2] == -1 and memo[i-5] == -1:
-                memo[i] = -1
-            elif memo[i-2] == -1:
-                memo[i] = memo[i-5] + 1
-            elif memo[i-5] == -1:
-                memo[i] = memo[i-2] + 1
-            else:
-                memo[i] = min(memo[i-5], memo[i-2]) + 1
-
-    # print(memo)
-    print(memo[n])
+    print(answer)
