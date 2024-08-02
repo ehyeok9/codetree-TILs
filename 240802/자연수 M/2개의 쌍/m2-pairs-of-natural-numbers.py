e21fed3d81423ad602739f0ef1a1493e383bee53
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-02 22:28:10 +0900

    Title: 자연수 M/2개의 쌍, Time: 373ms, Memory: 33MB, Status: Passed - CodeTree

--- 240802/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 240802/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -14,14 +14,17 @@ if __name__=="__main__":
     cur = 0
     answer = 0
     while cur <= pointer:
+        # print(cur, pointer)
         tmp = nums[cur][1] + nums[pointer][1]
         answer = max(tmp, answer)
         
-        nums[cur][0] -= 1
-        nums[pointer][0] -=1
+        minus = min(nums[cur][0], nums[pointer][0])
+        # print(minus, nums[cur][0], nums[pointer][0])
+        nums[cur][0] -= minus
+        nums[pointer][0] -= minus
 
-        if nums[cur][0] == 0: cur += 1
-        if nums[pointer][0] == 0: pointer -= 1
+        if nums[cur][0] <= 0: cur += 1
+        if nums[pointer][0] <= 0: pointer -= 1
 
     print(answer)
     # 가장 큰 값이랑 가장 작은 값을 더하자
