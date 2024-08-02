927a22b81042ef6477866e894b49dc1963f56fa0
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-02 21:53:39 +0900

    Title: 폭탄 해체 작업, Time: 209ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240802/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240802/폭탄 해체 작업/the-bomb-dismantling.py
@@ -3,31 +3,22 @@ from functools import cmp_to_key
 
 input = sys.stdin.readline
 
-def compare(x, y):
-    if x[1] < y[1]:
-        return -1
-    elif x[1] > y[1]:
-        return 1
-    else:
-        if x[0] > y[0]:
-            return -1
-        else:
-            return 1
-
 if __name__ == "__main__":
     n = int(input())
     score = [
         list(map(int, input().split()))
         for _ in range(n)
     ]
-    score.sort(cmp_to_key(compare))
-    
-    answer = score[0][0]
-    prev = score[0][1]
-    for i in range(1, n):
-        point, limit = score[i]
-        if prev == limit: continue
-        answer += point
-        prev = limit
+    visit = [0 for _ in range(10001)]
 
+    score.sort(key= lambda x : (-x[0],x[1]))
+    
+    answer = 0
+    for i in range(n):
+        point, lh = score[i]
+        extra = sum(visit[:i+2])
+        if extra <= lh:
+            answer += point
+            visit[lh] += 1
+    
     print(answer)
