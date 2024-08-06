7bbc4b2b56fb3e7ae2bd24d72d72356b1f2136ed
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-06 14:40:32 +0900

    Title: 적석과 흑석 2, Time: 1468ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240806/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -0,0 +1,39 @@
+import sys
+
+input = sys.stdin.readline
+
+def greedy(T, AB):
+    global c, n
+
+    AB.sort()
+    T.sort()
+
+    answer = 0
+    for black in AB:
+        start, end = black
+        val = None
+        for num in T:
+            if start <= num:
+                val = num
+                break
+
+        if val == None: continue
+        if start <= val <= end:
+            answer += 1
+            T.remove(val)
+            # print(start, val, end)
+    
+    print(answer)
+
+if __name__=="__main__":
+    c, n = map(int, input().split())
+    T = [
+        int(input())
+        for _ in range(c)
+    ]
+    AB = [
+        list(map(int, input().split()))
+        for _ in range(n)
+    ]
+
+    greedy(T, AB)
