dc7d2e8bd6c7f862d730d843f865b51034ab3c8b
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-17 16:51:14 +0900

    Title: 돌아가는 팔각 의자, Time: 150ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240817/돌아가는 팔각 의자/rounding-eight-angle.py
@@ -0,0 +1,62 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+def rotateClock(deq):
+    deq.appendleft(deq.pop())
+
+def rotateReversedClock(deq):
+    deq.append(deq.popleft())
+
+def judge(leftDeq, rightDeq):
+    if (leftDeq[2] != rightDeq[6]):
+        return True
+    return False
+
+def executeCommand(tables, tableNum, direction, visited):
+    if (tableNum < 0 or tableNum >= 4): return
+
+    if (visited[tableNum] == True): return
+    visited[tableNum] = True
+    # print(tableNum, direction)
+    if (tableNum == 0 and judge(tables[tableNum], tables[tableNum+1])):
+        executeCommand(tables, tableNum + 1, -direction, visited)
+    elif (tableNum == 3 and judge(tables[tableNum-1], tables[tableNum])):
+        executeCommand(tables, tableNum - 1, -direction, visited)
+    else:
+        if judge(tables[tableNum-1], tables[tableNum]):
+            executeCommand(tables, tableNum - 1, -direction, visited)
+        if judge(tables[tableNum], tables[tableNum+1]):
+            executeCommand(tables, tableNum + 1, -direction, visited)
+        
+
+    if (direction == 1): rotateClock(tables[tableNum])
+    else: rotateReversedClock(tables[tableNum])
+
+def calcResult(tables):
+    result = 0
+    for i in range(4):
+        result += (i+1) * tables[i][0]
+    return result
+    
+
+
+if __name__=="__main__":
+    tables = []
+    for i in range(4):
+        tables.append(
+            deque(map(int, input().strip()))
+        )
+    
+    k = int(input())
+    commands = [
+        list(map(int, input().split()))
+        for _ in range(k)
+    ]
+
+    for tableNum, direction in commands:
+        visited = [False] * 4
+        executeCommand(tables, tableNum-1, direction, visited)
+        # print(tables)
+    print(calcResult(tables))
